32c00d40a2f9b48487d5dce505029052bec94b08
BonnaA1 <BonnaA@uni.coventry.ac.uk>
2026-03-26 17:08:25 +0000

    Update ligth_device.py

--- devices/ligth_device.py
+++ devices/ligth_device.py
@@ -1,10 +1,10 @@
 def turn_on():
-    print("💡 Light Device: ON")
+    print("Light Device: ON")
  #Defines a function called turn on 
     #This represents - the action of switching the light ON
     #this prints a message on the console
 def turn_off():
-    print("🌑 Light Device: OFF")
+    print(" Light Device: OFF")
     #Defines another function called turn_off 
     #This represents - switching the lighbt OFF
     # prints light device is off
