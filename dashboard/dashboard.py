cfb1d1884f5451a2550ee1adb77bbb82b41cd155
Femi <adesinaiwp@coventry.ac.uk>
2026-04-13 00:52:25 +0100

    fix dashboard topic name and some misspelling

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,12 +1,12 @@
 from mqtt.mqtt_client import connect 
 import json 
 
-topic_dashnorad = "smart_oficce/security/dashboard"
+topic_dashboard= "smart_office/security/dashboard"
 client = connect() 
 
 def on_connect(client, userdata, flags, rc):
     print("Dashboard connected to broker")
-    client.subscribe(topic_dashnorad)
+    client.subscribe(topic_dashboard)
 
 def on_message(client, userdata, msg):
     data = json.loads(msg.payload.decode()) 
