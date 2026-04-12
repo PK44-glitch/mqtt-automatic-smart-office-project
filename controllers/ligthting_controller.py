d598ca0d1f5d12d5937306d50c5c9434d8bb7525
bonnaA1 <Bonnaa@coventry.ac.uk>
2026-04-12 20:37:29 +0100

    Updated controller to enable MQTT communication and device integration

--- controllers/ligthting_controller.py
+++ controllers/ligthting_controller.py
@@ -1,30 +1,33 @@
-# controllers/lighting_controller.py
-
-from devices.light_device import turn_on, turn_off
+import paho.mqtt.client as mqtt
+from devices.ligth_device import turn_on, turn_off
 
 light_level = None
-motion_detected = None
-
 
 def process_data(sensor_type, value):
-    global light_level, motion_detected
+    global light_level
 
     if sensor_type == "light":
         light_level = value
-
-    elif sensor_type == "motion":
-        motion_detected = value
-
-    handle_light()
-
+        handle_light()
 
 def handle_light():
-    if light_level is None or motion_detected is None:
+    if light_level is None:
         return
 
-    if light_level < 300 and motion_detected:
+    if light_level < 300:
         turn_on()
     else:
         turn_off()
-        #we have decided to not code in central controller toi test coide as it will be messy
-        #by keeping the code in seperate files logic stays in one place and central controller stays simple.
+
+def on_message(client, userdata, msg):
+    value = int(msg.payload.decode())
+    print("Received:", value)
+    process_data("light", value)
+
+client = mqtt.Client()
+client.on_message = on_message
+
+client.connect("broker.hivemq.com", 1883, 60)
+client.subscribe("office/light_level")
+
+client.loop_forever()
