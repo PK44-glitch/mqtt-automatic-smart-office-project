1e32dbf1f79053b4a80d08cff539d9ce9396a818
bonnaA1 <Bonnaa@coventry.ac.uk>
2026-04-13 03:12:10 +0100

    Resolved merge conflict and updated motion_sensor with working MQTT publishing

--- sensors/motion_sensor.py
+++ sensors/motion_sensor.py
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-import json 
-from datetime import datetime 
-from mqtt.mqtt_client import connect 
-
-topic_motion = "smart_office/security/motion"
-
-client = connect()
-client.loop_start()
-
-def send_motion():
-    message = { 
-       "motion_detected": True, 
-       "time": str(datetime.now())
-    }
-
-    client.publish(topic_motion, json.dumps(message))
-    print("Motion detected message sent")
-
-def run_sensor(): 
-    while True: 
-        user_input = input("press enter to simulate motion or type q to quit:")
-
-        if user_input.lower() == "q": 
-            break 
-
-        send_motion() 
-    client.disconnect()
-    print("Motion sensor stopped")
-
-if __name__ == "__main__": 
-    run_sensor() 
-
-
-=======
 import time
 import random
 from mqtt.mqtt_client import connect
@@ -41,12 +6,11 @@ client = connect()
 
 def publish_motion():
     while True:
-        motion = random.choice([0, 1])
+        motion = random.choice([0, 1])  # 0 = no motion, 1 = motion
         print(f"[Sensor] Motion: {motion}")
         
         client.publish("office/motion", motion)
-        time.sleep(7)
+        time.sleep(5)
 
 if __name__ == "__main__":
     publish_motion()
->>>>>>> 379b398 (Added smart lighting controller and device integration)
