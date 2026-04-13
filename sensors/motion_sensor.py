1fff8d9faf6e78297267d5b930b10bad06b6f95e
Femi <adesinaiwp@coventry.ac.uk>
2026-04-13 22:23:43 +0100

    refactor motion for main.py

--- sensors/motion_sensor.py
+++ sensors/motion_sensor.py
@@ -1,16 +1,52 @@
+from mqtt.mqtt_client import connect
+import json
+from datetime import datetime
 import time
 import random
-from mqtt.mqtt_client import connect
 
-client = connect()
+topic_security_motion = "smart_office/security/motion"
+topic_light_motion = "office/motion"
 
-def publish_motion():
+
+def send_security_motion(client):
+    message = {
+        "motion_detected": True,
+        "time": str(datetime.now())
+    }
+
+    client.publish(topic_security_motion, json.dumps(message))
+    print("Security motion detected message sent")
+
+
+def run_security_sensor():
+    client = connect()
+    client.loop_start()
+
     while True:
-        motion = random.choice([0, 1])  # 0 = no motion, 1 = motion
-        print(f"[Sensor] Motion: {motion}")
-        
-        client.publish("office/motion", motion)
+        user_input = input("Press Enter for security motion or type q to quit security sensor: ")
+
+        if user_input.lower() == "q":
+            break
+
+        send_security_motion(client)
+
+    client.disconnect()
+    print("Security motion sensor stopped")
+
+
+def run_lighting_sensor():
+    client = connect()
+
+    while True:
+        motion = random.choice([0, 1])
+        print("Lighting motion:", motion)
+        client.publish(topic_light_motion, motion)
         time.sleep(5)
 
+
+def run_sensor():
+    run_security_sensor()
+
+
 if __name__ == "__main__":
-    publish_motion()
+    run_sensor()
